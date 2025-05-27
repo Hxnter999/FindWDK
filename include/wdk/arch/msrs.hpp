6349b1ac163a67a69d82381372557dcbdcd485cf
Hxnter999 <78633943+Hxnter999@users.noreply.github.com>
2025-05-27 04:31:41 -0400

    merging more intel and amd structured, improved some msr member variable names

--- include/wdk/arch/msrs.hpp
+++ include/wdk/arch/msrs.hpp
@@ -9,26 +9,26 @@ namespace arch {
         using win::scalar_convertible<efer, std::uint64_t>::scalar_convertible;
         static constexpr std::uint32_t number = 0xC000'0080;
 
-        std::uint64_t syscall_extensions            : 1 {};
-        std::uint64_t reserved1                     : 7 {};
-        std::uint64_t long_mode                     : 1 {};
-        std::uint64_t reserved2                     : 1 {};
-        std::uint64_t long_mode_active              : 1 {};
-        std::uint64_t no_execute                    : 1 {};
-        std::uint64_t svm                           : 1 {};
-        std::uint64_t long_mode_segment_limit       : 1 {};
-        std::uint64_t fast_fxsave_fxrstor           : 1 {};
-        std::uint64_t translation_cache_extension   : 1 {};
-        std::uint64_t execute_only_pte              : 1 {}; // this is xotext, allows any given pte to be marked as execute only
-        std::uint64_t mcommit                       : 1 {};
-        std::uint64_t interruptible_wbinvd_wbnoinvd : 1 {};
-        std::uint64_t reserved3                     : 1 {};
-        std::uint64_t upper_address_ignore          : 1 {};
-        std::uint64_t auto_ibrs                     : 1 {};
-        std::uint64_t reserved4                     : 42 {};
+        std::uint64_t syscall_extensions                  : 1 {};
+        std::uint64_t reserved1                           : 7 {};
+        std::uint64_t long_mode                           : 1 {};
+        std::uint64_t reserved2                           : 1 {};
+        std::uint64_t long_mode_active                    : 1 {};
+        std::uint64_t no_execute                          : 1 {};
+        std::uint64_t amd64_svm                           : 1 {};
+        std::uint64_t amd64_long_mode_segment_limit       : 1 {};
+        std::uint64_t amd64_fast_fxsave_fxrstor           : 1 {};
+        std::uint64_t amd64_translation_cache_extension   : 1 {};
+        std::uint64_t amd64_execute_only_pte              : 1 {}; // this is xotext, allows any given pte to be marked as execute only
+        std::uint64_t amd64_mcommit                       : 1 {};
+        std::uint64_t amd64_interruptible_wbinvd_wbnoinvd : 1 {};
+        std::uint64_t reserved3                           : 1 {};
+        std::uint64_t amd64_upper_address_ignore          : 1 {};
+        std::uint64_t amd64_auto_ibrs                     : 1 {};
+        std::uint64_t reserved4                           : 42 {};
     };
 
-    // virtual machine host save physical address as per the manual
+    // virtual machine host save physical address as per the AMD manual
     struct vmhs_pa : win::scalar_convertible<vmhs_pa, std::uint64_t> {
         using win::scalar_convertible<vmhs_pa, std::uint64_t>::scalar_convertible;
         static constexpr std::uint32_t number = 0xC001'0117;
@@ -43,12 +43,26 @@ namespace arch {
         std::uint64_t value{};
     };
 
+    struct cstar : win::scalar_convertible<lstar, std::uint64_t> {
+        using win::scalar_convertible<lstar, std::uint64_t>::scalar_convertible;
+        static constexpr std::uint32_t number = 0xC000'0083;
+
+        std::uint64_t value{};
+    };
+
+    struct syscall_flag_mask : win::scalar_convertible<lstar, std::uint64_t> {
+        using win::scalar_convertible<lstar, std::uint64_t>::scalar_convertible;
+        static constexpr std::uint32_t number = 0xC000'0084;
+
+        std::uint64_t value{};
+    };
+
     // max performance
     struct mperf : win::scalar_convertible<mperf, std::uint64_t> {
         using win::scalar_convertible<mperf, std::uint64_t>::scalar_convertible;
         static constexpr std::uint32_t number = 0xC000'00E7;
 
-        std::uint64_t value{};
+        std::uint64_t max_core_clocks_counter{};
     };
 
     // actual performance
@@ -56,7 +70,7 @@ namespace arch {
         using win::scalar_convertible<aperf, std::uint64_t>::scalar_convertible;
         static constexpr std::uint32_t number = 0xC000'00E8;
 
-        std::uint64_t value{};
+        std::uint64_t actual_core_clocks_counter{};
     };
 
     // instructions retired
@@ -64,8 +78,8 @@ namespace arch {
         using win::scalar_convertible<irperf, std::uint64_t>::scalar_convertible;
         static constexpr std::uint32_t number = 0xC000'00E9;
 
-        std::uint64_t value    : 48 {};
-        std::uint64_t reserved : 16 {};
+        std::uint64_t instructions_retired_counter : 48 {};
+        std::uint64_t reserved                     : 16 {};
     };
 
     struct fs_base : win::scalar_convertible<fs_base, std::uint64_t> {
@@ -87,6 +101,14 @@ namespace arch {
         static constexpr std::uint32_t number = 0xC000'0102;
 
         std::uint64_t value{};
+    };
+
+    struct auxilary_tsc : win::scalar_convertible<kernel_gs_base, std::uint64_t> {
+        using win::scalar_convertible<kernel_gs_base, std::uint64_t>::scalar_convertible;
+        static constexpr std::uint32_t number = 0xC000'0103;
+
+        std::uint64_t auxilary_tsc_data : 32 {};
+        std::uint64_t reserved          : 32 {};
     };
 } // namespace arch
 
